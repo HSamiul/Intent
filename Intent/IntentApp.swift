af592fd28de53a88ff23ecf2659f470403211047
HSamiul <samiulh@andrew.cmu.edu>
2022-08-10 23:41:38 -0400

    Initial Commit

---
+++ Intent/IntentApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntentApp.swift
+//  Intent
+//
+//  Created by Samiul Hoque on 8/10/22.
+//
+
+import SwiftUI
+
+@main
+struct IntentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
